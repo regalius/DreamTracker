fccd5b5e4eb92d6c930891202b3f17c91238a094
Nakama <nakama@200293-imac.local>
2018-10-19 08:25:20 +0700

    Initial Commit

---
+++ DreamTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DreamTracker
+//
+//  Created by Nakama on 10/19/18.
+//  Copyright © 2018 Nakama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
